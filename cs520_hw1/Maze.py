57074eb10ba7b61e2f367f95691a4ff37d8a8087
Yang Xiao <skzr001@outlook.com>
2017-09-26 23:56:47 -0400

    change variable name

--- cs520_hw1/Maze.py
+++ cs520_hw1/Maze.py
@@ -19,10 +19,10 @@ def build_maze(maze_matrix):
     for i in range(maze_high):
         for j in range(maze_width):
             if maze_matrix[i][j] == 0:
-                edgeN = ((i, j), 'Up')
-                edgeS = ((i + 1, j), 'Down')
-                edgeW = ((i, j), 'Left')
-                edgeE = ((i, j + 1), 'Right')
+                edge_up = ((i, j), 'Up')
+                edgeS_down = ((i + 1, j), 'Down')
+                edge_left = ((i, j), 'Left')
+                edge_right = ((i, j + 1), 'Right')
                 if i < maze_high - 1 and maze_matrix[i + 1][j] == 0:
                     maze_dict[(i, j)].append(edgeS)
                     maze_dict[(i + 1, j)].append(edgeN)
