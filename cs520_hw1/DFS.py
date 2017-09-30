5cde80cf45c986513f398a38d020aa9bde3dad0a
Yang Xiao <skzr001@outlook.com>
2017-09-30 16:05:02 -0400

    add max_path_length,max_fringe_length

--- cs520_hw1/DFS.py
+++ cs520_hw1/DFS.py
@@ -3,11 +3,23 @@ import numpy as np
 
 def dfs(maze_dict, dim, maze_matrix):
     maze_matrix_dfs_visited = np.copy(maze_matrix)
+
+    path_dict = {}
+    path_dict[(0, 0)] = (0, 0)
+
     visited, stack = set(), [(0, 0)]
     destination = (dim-1, dim-1)
+    result = dict()
+
+    # set required return value for HW1
+    result["status"] = -1
+    max_fringe = 0
+    num_fringe = 1
 
     while stack:
+        max_fringe = max(max_fringe, num_fringe)
         vertex = stack.pop()
+        num_fringe -= 1
 
         if vertex not in visited:
             visited.add(vertex)
@@ -16,12 +28,45 @@ def dfs(maze_dict, dim, maze_matrix):
             continue
 
         if vertex == destination:
-            print ("DFS has reached the destination")
-            print ("Number of vertices visited: " + str(len(visited)))
+            #path_dict[destination] = vertex  #  this stupid mistake waste my whole night. Damn it!
+            print ("Congratulations! DFS has reached the destination!")
+            result["maze_path"] = find_path(path_dict, destination)
+            result["maze_max_length"] = len(result["maze_path"])
+            #print ("Maze path is: ")
+            print result["maze_path"]
+            print ("Maze Path length is: " + str(len(result["maze_path"])))
+            result["tree_size"] = str(len(visited))
+            result["max_fringe"] = max_fringe
+            result["maze_matrix_visited"] = maze_matrix_dfs_visited
+
+            print ("Number of vertices visited: " + result["tree_size"])
             return maze_matrix_dfs_visited
 
         for child, path in maze_dict[vertex]:
             if child not in visited:
+                num_fringe += 1
+                path_dict[child] = vertex
+                #print ("vertex")
+                #print vertex
+                #print ("child")
+                #print child
+                #print ("path_dict[child]")
+                #print path_dict[child]
                 stack.append(child)
 
-    return "No result!"
+    return "No Result!"
+
+
+def find_path(path_dict, destination):
+    path = list()
+    path.append(destination)
+    current_node = destination
+
+    while current_node != (0, 0):
+        #print ("current node" + str(current_node))
+        current_node = path_dict[current_node]
+        #print ("Parent of current node" + str(current_node))
+        path.append(current_node)
+
+    return path
+
