62a357fe9d4eb63b3dc73de5db69f62c3bdec51d
Noble <guolei1.chen@gmail.com>
2017-09-30 22:29:18 -0400

    add max fringe and length

--- cs520_hw1/AStar_Manhattan.py
+++ cs520_hw1/AStar_Manhattan.py
@@ -1,9 +1,60 @@
 import numpy as np
 import math
+import heapq
 
-def dist_manhattan(a, b):
-    a_x, a_y = a
-    b_x, b_y = b
+# Manhattan distance
+def dist_Manhattan(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
 
-    return math.fabs(a_x-b_x) + math.fabs(a_y-b_y)
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+    def __getitem__(self, key):
+        return self[key]
+
+    def __len__(self):
+        return len(self.elements)
+
+
+def a_star_search(maze_dict, dim, maze_matrix):
+    maze_matrix_AM_visited = np.copy(maze_matrix)
+    visited = PriorityQueue()
+    visited.put((0, 0), 0)
+    destination = (dim - 1, dim - 1)
+    came_from = {}
+    cost_so_far = {}
+    came_from[(0, 0)] = None
+    cost_so_far[(0, 0)] = 0
+    count = 0
+    while not visited.empty():
+        current = visited.get()
+
+        if current == destination:
+            print ("A* with Manhattan distance has reached the destination")
+            print ("Number of vertices visited: " + str(count+1))
+            return maze_matrix_AM_visited
+
+        for child, path in maze_dict[current]:
+            new_cost = cost_so_far[current] + 1
+            if child not in cost_so_far or new_cost < cost_so_far[child]:
+                cost_so_far[child] = new_cost
+                priority = new_cost + dist_Manhattan(destination, child)
+                visited.put(child, priority)
+                count += 1
+                maze_matrix_AM_visited[child] = -1
+
 
